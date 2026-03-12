8480c0f316c3f97c673874f080b8ba5da39d1a45
gtorresn21-hub <gtorresn21@campusuninunez.edu.co>
2026-03-12 12:59:12 -0500

    Add files via upload

--- ejercicio 3.py
+++ ejercicio 3.py
@@ -1,2 +1,9 @@
-def analizar_numeros (*arg, *kwargs)
-    return arg
+def analizar_numeros (*args, *kwargs)
+    
+    pares = []
+    impares = []
+
+    for num in args: 
+    if num % 2 == 0:
+
+    pares.append = num
